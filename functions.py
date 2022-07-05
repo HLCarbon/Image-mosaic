b5342f2f01a55933df63fd6ea8c3194f12dfbdd3
Henrique Lima <97365179+HLCarbon@users.noreply.github.com>
2022-07-05 11:25:26 +0100

    Added typing

--- functions.py
+++ functions.py
@@ -6,7 +6,7 @@ import sys
 
 
 
-def reshape_photo_to_array(photo):
+def reshape_photo_to_array(photo:Image.Image) ->np.array:
     width, height = photo.size
     photo = photo.convert('RGB')
     photo2d_array = np.array(photo.getdata())
@@ -17,7 +17,7 @@ def reshape_photo_to_array(photo):
     #print('The new shape is:',image_array.shape)
     return image_array
 
-def new_shape(desired_image, n_photos):
+def new_shape(desired_image: Image.Image, n_photos:int) ->int,int,tuple,Image.Image:
     width, height = desired_image.size
     proportion = width/height
     square = n_photos**(1/2)
@@ -30,7 +30,7 @@ def new_shape(desired_image, n_photos):
 
 
 
-def reshape_all_photos(initial_photos, shape, x, y):
+def reshape_all_photos(initial_photos:list, shape:tuple, x:int, y:int)-> np.array:
     photos_in_array = []
     n = 0
     for image in initial_photos:
@@ -45,9 +45,9 @@ def reshape_all_photos(initial_photos, shape, x, y):
         
         photos_in_array.append(image_array)
         n +=1
-    return photos_in_array
+    return np.array(photos_in_array)
 
-def create_joined_photo(desired_photo, lst_of_photos, shape, x, y):
+def create_joined_photo(desired_photo:Image.Image, array_of_photos:np.array, shape:tuple, x:int, y:int)->np.array:
 
     n_for_x = int(shape[0]/x)
     n_for_y = int(shape[1]/y)
@@ -57,15 +57,14 @@ def create_joined_photo(desired_photo, lst_of_photos, shape, x, y):
     for a in range(x):
         for b in range(y):
             for c in range(n_for_y):
-                new_photo[b*n_for_y+c][(n_for_x)*a:(a+1)*(n_for_x)] = lst_of_photos[b*x+a][c]
+                new_photo[b*n_for_y+c][(n_for_x)*a:(a+1)*(n_for_x)] = array_of_photos[b*x+a][c]
     return new_photo
 
-def create_photo(photo, joined_photos, proportion):
+def create_photo(photo:np.array, joined_photos:np.array, proportion:float)->np.array:
     final = photo*proportion + joined_photos*(1-proportion)
     return final
 
-def save_image(image, file_name):
-    final_array = np.array(image)
+def save_image(image:np.array, file_name:str) -> None:
     final_array = final_array.astype(np.uint8)
     new_image = Image.fromarray(final_array)
     new_image.save(f'final_image/{file_name}.png')
